838e173d52bc73376e9741e31a2107be4cf714a8
Madhav Chaturvedi <madhavchaturvedi0562@gmail.com>
2025-06-30 00:54:47 +0530

    Made the CRUD API's

--- app.js
+++ app.js
@@ -3,15 +3,10 @@ const connectDB = require("./config/database");
 const User = require("./models/User");
 const app = express();
 
-app.post("/signup", async (req, res) => {
-  const userObj = {
-    firstName: "Madhav",
-    lastName: "Chaturvedi",
-    emailId: "madhavchaturvedi0562@gmail.com",
-    password: "madhav@1234",
-  };
+app.use(express.json());
 
-  const newUser = new User(userObj);
+app.post("/signup", async (req, res) => {
+  const newUser = new User(req.body);
 
   try {
     await newUser.save();
@@ -21,11 +16,46 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+// Feed API - get all the users
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.send("No users Found!!");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(401).send("Something went Wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User is successfull Deleted!");
+  } catch (err) {
+    res.status(401).send("Something went Wrong");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    const data = req.body;
+    const user = await User.findByIdAndUpdate(userId, data);
+    res.send("user successfully Updated!");
+  } catch (err) {
+    res.status(401).send("Something went Wrong");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database connection established...");
     app.listen(8000, () => {
-      console.log("server is sucessfully running on 8000");
+      console.log("server is successfully running on 8000");
     });
   })
   .catch((err) => {
