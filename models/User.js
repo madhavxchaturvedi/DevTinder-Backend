aa072283e23134589baab0f3287cb807657c8043
Madhav Chaturvedi <madhavchaturvedi0562@gmail.com>
2025-07-04 00:20:31 +0530

    Validate Schema by Validator Lib

--- models/User.js
+++ models/User.js
@@ -1,50 +1,69 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-  },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-    required: true,
-    lowercase: true,
-    unique: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minLength: 8,
-  },
-  age: {
-    type: String,
-    minmum: 16,
-  },
-  gender: {
-    type: String,
-    validate(value) {
-      if (!["male", "female", "other"].includes(value)) {
-        throw new Error("Gender data is not valid!");
-      }
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid Email address: " + value);
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 8,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a Strong Password " + value);
+        }
+      },
+    },
+    age: {
+      type: String,
+      minmum: 16,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "other"].includes(value)) {
+          throw new Error("Gender data is not valid!");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default: "https://geographyandyou.com/images/user-profile.png",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid Photo URL: " + value);
+        }
+      },
+    },
+    about: {
+      type: String,
+    },
+    skills: {
+      type: [String],
     },
   },
-  photoUrl: {
-    type: String,
-    default: "https://geographyandyou.com/images/user-profile.png",
-  },
-  about: {
-    type: String,
-  },
-  skills: {
-    type: [String],
-  },
-},{
-  timestamps:true,
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
