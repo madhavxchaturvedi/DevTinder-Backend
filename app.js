9e0bd8b6cbc3436e214e2c11ef3a511eedc1d620
Madhav Chaturvedi <madhavchaturvedi0562@gmail.com>
2025-07-06 00:11:55 +0530

    Cookies, JWT-Token

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const connectDB = require("./config/database");
 const User = require("./models/User");
 const { validateSignUpData } = require("./utils/validation");
 const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.post("/signup", async (req, res) => {
   try {
@@ -41,10 +45,38 @@ app.post("/login", async (req, res) => {
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (isPasswordValid) {
+      const token = await jwt.sign({ _id: user._id }, "devtinder@2006");
+
+      res.cookie("token", token);
+
       res.send("Login Successfully!!");
     } else {
       throw new Error("Invalid Credentials");
     }
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
+
+app.get("/profile", async (req, res) => {
+  try {
+    const cookies = req.cookies;
+    const { token } = cookies;
+
+    if (!token) {
+      throw new Error("Invalid Token");
+    }
+
+    const decodedMessage = await jwt.verify(token, "devtinder@2006");
+
+    const { _id } = decodedMessage;
+    const user = await User.findById(_id);
+
+    if (!user) {
+      throw new Error("User does not exist");
+    }
+
+    res.send(user);
   } catch (err) {
     res.status(400).send("ERROR : " + err.message);
   }
