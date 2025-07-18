7ed0a7419ceb8b95efb791c753229d9f0b827ad2
Madhav Chaturvedi <madhavchaturvedi0562@gmail.com>
2025-07-19 02:13:38 +0530

    Fetching connections API

--- routes/user.js
+++ routes/user.js
@@ -28,5 +28,28 @@ router.get("/user/request/received", userAuth, async (req, res) => {
   }
 });
 
+router.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequest = await ConnectionRequestModel.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    }).populate("fromUserId", userAllowedData);
+
+    const data = connectionRequest.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
+
+    res.json({ data });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 module.exports = router;
